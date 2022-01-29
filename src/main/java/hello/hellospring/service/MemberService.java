b197072376a743683d3f770b671246fe167fb9a9
cksal0805 <valley@smartstudy.co.kr>
2022-01-29 19:09:59 +0900

    Update 자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
